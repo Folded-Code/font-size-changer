f2aca0a6e6c569c55df2649a0ea6aae91617eb08
Folded_Code <34174315+Folded-Code@users.noreply.github.com>
2019-12-29 15:20:51 +0000

    Added git

---
+++ spec/font-size-changer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FontSizeChangerView from '../lib/font-size-changer-view';
+
+describe('FontSizeChangerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
